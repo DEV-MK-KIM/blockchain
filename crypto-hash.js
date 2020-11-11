e59ed94057bd0f5615b75cfa8ec03e14800cde05
MK Kim <mk.kim.yyc@gmail.com>
2020-11-10 22:49:31 -0700

    init

---
+++ crypto-hash.js
@@ -0,0 +1,10 @@
+const crypto = require("crypto");
+
+const cryptoHash = (...inputs) => {
+  const hash = crypto.createHash("sha256");
+  console.log(inputs)
+  hash.update(inputs.sort().join(" "));
+  return hash.digest('hex')
+};
+
+module.exports = cryptoHash;
