8f219acb64db39f907b3857fdc4272c49562520f
MK Kim <mk.kim.yyc@gmail.com>
2020-11-21 23:50:22 -0700

    finished publish/sub section

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
+const request = require("request");
 
 const app = express();
-const blocckhain = new Blockchain();
+const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
 
-app.use(bodyParser.json())
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+// setTimeout(() => pubsub.broadcastChain(), 1000);
+
+app.use(bodyParser.json());
 
 app.get("/api/blocks", (req, res) => {
-  res.json(blocckhain.chain);
+  res.json(blockchain.chain);
 });
 
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
-  blocckhain.addBlock({ data });
-  
-  res.redirect('/api/blocks')
+  blockchain.addBlock({ data });
+
+  pubsub.broadcastChain();
+
+  res.redirect("/api/blocks");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log("replace chain on a sync with :>> ", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => {
+  console.log(`listening at localhost:${PORT}`);
+
+  if( PORT !== DEFAULT_PORT) syncChains()
+});
